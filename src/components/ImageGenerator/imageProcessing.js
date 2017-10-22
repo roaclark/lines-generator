f51c1de0be5c9a040a4d7a438b11ee42b34763fe
Rachel Rosalia <rachel@rosalia.me>
2017-10-22 13:02:48 -0700

    Implement getting vertices for the image graph

--- src/components/ImageGenerator/imageProcessing.js
+++ src/components/ImageGenerator/imageProcessing.js
@@ -1,5 +1,28 @@
+const getCoordinatesList = (width, height) => {
+  const xs = Array.from(Array(height).keys())
+  const ys = Array.from(Array(width).keys())
+  return ys.map(y => xs.map(x => [x, y])).reduce((a, b) => a.concat(b), [])
+}
+
+const usePixel = (x, y, r, g, b, a) => {
+  return a > 0
+}
+
+const getRGBA = (x, y, imageData) => {
+  const offset = x * imageData.width + y
+  const rgba = imageData.data.slice(offset * 4, offset * 4 + 4)
+  return rgba
+}
+
 const convertImageToGraph = async imageData => {
-  return imageData.data
+  const vertices = getCoordinatesList(
+    imageData.width,
+    imageData.height,
+  ).filter(pixel => {
+    const [r, g, b, a] = getRGBA(pixel[0], pixel[1], imageData)
+    return usePixel(pixel[0], pixel[1], r, g, b, a)
+  })
+  return vertices
 }
 
 export { convertImageToGraph }
